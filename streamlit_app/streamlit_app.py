6a24142be677b3f0839d2a5c2e452010296dbaf0
Christophe Gakwaya <c.gakwaya@alustudent.com>
2025-08-03 13:24:39 +0200

    updated

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(
 )
 
 # --- Constants ---
-API_URL = "https://vehicle-classifier-api.onrender.com/"
+API_URL = "https://vehicle-classifier-api.onrender.com"
 PREDICT_ENDPOINT = f"{API_URL}/predict"
 RETRAIN_ENDPOINT = f"{API_URL}/retrain"
 
